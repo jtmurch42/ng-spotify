d397c132f2d2087181656f0ce709e2ac44354d2a
Jeremy Murchison <jtmurch92@gmail.com>
2021-08-08 12:02:39 -0700

    Remove middleware function and querystring module

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
 const express = require('express');
 const path = require('path');
 const axios = require('axios');
-const querystring = require('querystring');
 
 const app = express();
 
 app.use('/', express.static('ng-spotify-ui/dist'));
-app.use(function (req, res, next) {
-  if (req.ip !== '::1' && req.ip !== '::ffff:127.0.0.1') {
-    res.status(401);
-    return res.send('Permission denied');
-  }
-  next();
-});
 
 app.get('/api/token', function (req, res) {
   axios
-    .post('https://accounts.spotify.com/api/token', querystring.stringify({ grant_type: 'client_credentials' }), {
+    .post('https://accounts.spotify.com/api/token', 'grant_type=client_credentials', {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
         Authorization: `Basic ${Buffer.from(
