ffd0e5ef39fc8fa3fbda95e0088c7d72ec7426ab
Jeremy Murchison <jtmurch92@gmail.com>
2022-01-14 02:10:14 -0700

    Update package files and karma file

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require("path").join(__dirname, "./coverage/ng-spotify-ui"),
+      dir: require("path").join(__dirname, "./coverage/ng-spotify"),
       reports: ["html", "lcovonly", "text-summary"],
       fixWebpackSourcePaths: true
     },
